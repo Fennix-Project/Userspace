6eb4bdd6f53a00b329191442eba8781664b0dd48
Alex <enderice2@users.noreply.github.com>
2022-12-25 22:47:47 +0200

    ELF Interpreter stub

--- libc/Interpreter/ld.c
+++ libc/Interpreter/ld.c
@@ -1,7 +1,12 @@
 #include "ld.h"
 
+/* TODO: Implement ELF interpreter. Currently it's implemented in the kernel. */
 int main(int argc, char *argv[], char *envp[])
 {
+    __asm__ __volatile__("syscall"
+                         :
+                         : "a"(1), "D"('H'), "S"(0)
+                         : "rcx", "r11", "memory");
     if (argc < 2)
         return -1;
 
