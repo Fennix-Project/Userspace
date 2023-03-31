b63459b7f3f16273dfa82e75e0256b085f8f750f
Alex <enderice2@users.noreply.github.com>
2023-03-31 17:38:33 +0300

    Update KCtl enum

--- libs/include/sysbase.h
+++ libs/include/sysbase.h
@@ -6,12 +6,39 @@
 enum KCtl
 {
     KCTL_NULL,
+
     KCTL_GET_PID,
     KCTL_GET_TID,
     KCTL_GET_UID,
     KCTL_GET_GID,
+
+    /**
+     * @brief Get the page size
+     */
     KCTL_GET_PAGE_SIZE,
+
+    /**
+     * @brief Check whether the current thread is critical
+     */
     KCTL_IS_CRITICAL,
+
+    /**
+     * @brief Register an ELF library
+     * @fn int RegisterELFLib(char *Identifier, char *Path)
+     */
+    KCTL_REGISTER_ELF_LIB,
+
+    /**
+     * @brief Get an ELF library
+     * @fn uintptr_t GetELFLib(char *Identifier);
+     */
+    KCTL_GET_ELF_LIB_FILE,
+
+    /**
+     * @brief Get an ELF library
+     * @fn uintptr_t GetELFLib(char *Identifier);
+     */
+    KCTL_GET_ELF_LIB_MEMORY_IMAGE,
 };
 
 long DoCtl(uint64_t Command, uint64_t Arg1, uint64_t Arg2, uint64_t Arg3, uint64_t Arg4);
