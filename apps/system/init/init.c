faa781a9ca36b2c1c9cbe8cd803c008ee456c2d7
Alex <enderice2@users.noreply.github.com>
2023-04-23 22:18:14 +0300

    Cleanup init

--- apps/system/init/init.c
+++ apps/system/init/init.c
@@ -1,21 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <aux.h>
 
-#include <sysbase.h>
-#include <sysfile.h>
 #include <init.h>
-
-void PutCharToKernelConsole(char c)
-{
-    __asm__ __volatile__("syscall"
-                         :
-                         : "a"(1), "D"(c), "S"(0)
-                         : "rcx", "r11", "memory");
-}
-
 #define print(m, ...) init_log(m, ##__VA_ARGS__)
 
-int main(int argc, char *argv[], char *envp[])
+void test_args(int argc, char *argv[], char *envp[])
 {
     print("%p %p %p\n", (void *)(uint64_t)&argc, (void *)&argv, (void *)&envp);
     print("I have %d arguments\n", argc);
@@ -38,8 +28,12 @@ int main(int argc, char *argv[], char *envp[])
 
     for (auxv = (Elf64_auxv_t *)e; auxv->a_type != AT_NULL; auxv++)
         print("auxv: %ld %#lx\n", auxv->a_type, auxv->a_un.a_val);
+}
 
-    File *test = FileOpen("/Test.txt", FILE_READ);
+int main(int argc, char *argv[], char *envp[])
+{
+    // test_args(argc, argv, envp);
+    FILE *test = fopen("/Test.txt", "r");
     if (test == NULL)
     {
         print("Failed to open file\n");
@@ -47,9 +41,9 @@ int main(int argc, char *argv[], char *envp[])
     }
 
     char buf[1024];
-    uint64_t read = FileRead(test, 0, (uint8_t *)buf, 1024);
+    int read = fread(buf, 1024, 1, test);
     print("Read %ld bytes from file\n", read);
     print("File contents: %s\n", buf);
-    FileClose(test);
+    fclose(test);
     return 0;
 }
